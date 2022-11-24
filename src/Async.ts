92dd7a78c117a5d0e9f6914f007ad42d9567e5bc
nikita <petrov.nikita702@mail.ru>
2022-11-24 15:30:41 +0300

    Задание

--- src/Async.ts
+++ src/Async.ts
@@ -3,6 +3,10 @@
 который завершится через заданное количество миллисекунд со значением, переданным в аргумент.
  */
 export function mock(ms: number): Promise<number> {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, ms);
+        resolve(ms);
+    });
 }
 
 /*
@@ -11,28 +15,24 @@ export function mock(ms: number): Promise<number> {
 export function getData(): Promise<number[]> {
     const result: number[] = [];
 
-    return mock(100)
-        .then((data1) => {
+    return Promise.all([mock(100), mock(200), mock(300)]).then(
+        ([data1, data2, data3]) => {
             result.push(data1);
-            return mock(200);
-        })
-        .then((data2) => {
             result.push(data2);
-            return mock(300);
-        })
-        .then((data3) => {
             result.push(data3);
             return result;
-        });
+        },
+    );
 }
 
 /*
 Исправьте функцию catchException так, чтобы блок try/catch обрабатывал
 завершенный с ошибкой Promise и возвращал текст ошибки.
  */
+
 export async function catchException(): Promise<string | undefined> {
     try {
-        Promise.reject(new Error('my error'));
+        await Promise.reject(new Error('my error'));
     } catch (err) {
         return err.message;
     }
